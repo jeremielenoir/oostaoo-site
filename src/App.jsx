c234808247f2a82f56af3275856130036d8c832f
SylvainOostaoo <laborderie@gmail.com>
2020-07-10 15:14:33 +0200

    trying to fix git trigger bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import Nav from './components/Nav';
-import Header from './components/Header';
-import Introduction from './components/Introduction';
-import Services from './components/Services';
-import Technos from './components/Technos';
-import References from './components/References';
-import Contact from './components/Contact';
-import CarouselNew from './components/CarouselNew';
+import React from "react";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Introduction from "./components/Introduction";
+import Services from "./components/Services";
+import Technos from "./components/Technos";
+import References from "./components/References";
+import Contact from "./components/Contact";
+import CarouselNew from "./components/CarouselNew";
 
-
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <>
-    <Nav/>
-    <Header id='accueil'/>
-    <Introduction/>
-    <Services id='services'/>
-    <Technos id='technos'/>
-    <References id='references'/>
-    <CarouselNew id='job'/>
-    <Contact id='contact'/>
-</>
+      <div style={{ fontSize: "200px", backgroundColor: "red" }}>
+        Hello trigger 4 !
+      </div>
+      <Nav />
+      <Header id="accueil" />
+      <Introduction />
+      <Services id="services" />
+      <Technos id="technos" />
+      <References id="references" />
+      <CarouselNew id="job" />
+      <Contact id="contact" />
+    </>
   );
 }
 
