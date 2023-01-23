f6b7efd4c72d47266290000bbda6e461d1b098e3
pablo <prietopablo88@gmail.com>
2023-01-23 14:38:01 +0100

    static JobOffers responsive with flex

--- components/JobOffers/JobOffers.tsx
+++ components/JobOffers/JobOffers.tsx
@@ -7,26 +7,67 @@ import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 
 const JobOffers = () => {
   return (
-    <section>
+    <section className={styles.offersSection} id="offres">
       <h2>Nos offres d'emploi</h2>
-      <section className={styles.offer}>
-        <header className={styles.offerHeader}>
-          <span className={styles.offerIconWrapper}>
-            <WorkOutlineIcon className={styles.offerIcon} style={{fill: "white"}} />
-          </span>
-          <div>
-            <h3 className={styles.offerTitle}>Dev Lead</h3>
-            <span className={styles.offerAvailability}>Disponible</span>
-          </div>
-        </header>
-          <Image className={styles.offerImage} src="" alt="job offer visual" height={250} />
-          <p className={styles.offerDescription}>
-            description description description description description description description description description description description description description description description description
-          </p>
-          <a className={styles.offerLink} href="#">
-            <Image src={logoLinkedIn} alt="LinkedIn link" />
-          </a>
-      </section>
+      <p>
+         tout cracheux ipsum dolor sit amet consectetur, adipisicing elit. Sequi alias iste ducimus tenetur saepe reprehenderit quasi reiciendis ab architecto.
+      </p>
+      <div className={styles.offerList}>
+        <section className={styles.offer}>
+          <header className={styles.offerHeader}>
+            <span className={styles.offerIconWrapper}>
+              <WorkOutlineIcon className={styles.offerIcon} style={{fill: "white"}} />
+            </span>
+            <div>
+              <h3 className={styles.offerTitle}>Dev Lead</h3>
+              <span className={styles.offerAvailability}>Disponible</span>
+            </div>
+          </header>
+            <Image className={styles.offerImage} src="" alt="job offer visual" height={200} />
+            <p className={styles.offerDescription}>
+              description description description description description description description description description description description description description description description description
+            </p>
+            <a className={styles.offerLink} href="#">
+              <Image src={logoLinkedIn} alt="LinkedIn link" />
+            </a>
+        </section>
+        <section className={styles.offer}>
+          <header className={styles.offerHeader}>
+            <span className={styles.offerIconWrapper}>
+              <WorkOutlineIcon className={styles.offerIcon} style={{fill: "white"}} />
+            </span>
+            <div>
+              <h3 className={styles.offerTitle}>Dev Lead</h3>
+              <span className={styles.offerAvailability}>Disponible</span>
+            </div>
+          </header>
+            <Image className={styles.offerImage} src="" alt="job offer visual" height={200} />
+            <p className={styles.offerDescription}>
+              description description description description description description description description description description description description description description description description
+            </p>
+            <a className={styles.offerLink} href="#">
+              <Image src={logoLinkedIn} alt="LinkedIn link" />
+            </a>
+        </section>
+        <section className={styles.offer}>
+          <header className={styles.offerHeader}>
+            <span className={styles.offerIconWrapper}>
+              <WorkOutlineIcon className={styles.offerIcon} style={{fill: "white"}} />
+            </span>
+            <div>
+              <h3 className={styles.offerTitle}>Dev Lead</h3>
+              <span className={styles.offerAvailability}>Disponible</span>
+            </div>
+          </header>
+            <Image className={styles.offerImage} src="" alt="job offer visual" height={200} />
+            <p className={styles.offerDescription}>
+              description description description description description description description description description description description description description description description description
+            </p>
+            <a className={styles.offerLink} href="#">
+              <Image src={logoLinkedIn} alt="LinkedIn link" />
+            </a>
+        </section>
+      </div>
     </section>
   )
 }
