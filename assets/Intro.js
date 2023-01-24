e0e87264143466c59b72ebbecb784be9f3b9c957
Julien-Allard <julalldev@gmail.com>
2023-01-24 14:28:54 +0100

    Feature/New component + Intro.js

---
+++ assets/Intro.js
@@ -0,0 +1,12 @@
+export const introElements = [
+   {
+      number: '01',
+      title: 'talents',
+      text: "Dévelopeur expérimenté et polyvalent, acteur responsable majeur des projets qui s'adonne à la coordination et à l'accompagnement de son équipe de dévelopeur à l'aide de ses connaissance technique précises.",
+   },
+   {
+      number: '02',
+      title: 'missions',
+      text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima blanditiis ea, quia, magnam ducimus architecto provident quo nulla enim, nemo nihil repellendus tenetur nam. Dolores vero nihil unde quidem quod.',
+   },
+];
--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -1,28 +1,20 @@
 import { FC } from 'react';
 
 import styles from './Intro.module.css';
+import { introElements } from '../../assets/Intro';
 
 const Intro: FC = () => {
    return (
       <div className={styles.container}>
-         <div className={styles.subContainer}>
-            <h1>01</h1>
-            <h2>TALENTS</h2>
-            <p>
-               Dévelopeur expérimenté et polyvalent, acteur responsable majeur des projets qui
-               s&apos;adonne à la coordination et à l&apos;accompagnement de son équipe de
-               dévelopeur à l&apos;aide de ses connaissance technique précises.
-            </p>
-         </div>
-         <div className={styles.subContainer}>
-            <h1>02</h1>
-            <h2>MISSIONS</h2>
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima blanditiis ea, quia,
-               magnam ducimus architecto provident quo nulla enim, nemo nihil repellendus tenetur
-               nam. Dolores vero nihil unde quidem quod.
-            </p>
-         </div>
+         {introElements.map((element) => {
+            return (
+               <div className={styles.subContainer}>
+                  <h1>{element.number}</h1>
+                  <h2>{element.title.toUpperCase()}</h2>
+                  <p>{element.text}</p>
+               </div>
+            );
+         })}
       </div>
    );
 };
