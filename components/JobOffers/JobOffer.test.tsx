a33ea75ccf2168509d21f28886da0c07e0eae4d4
Paul Rigaudeau <paul.rigaudeau@me.com>
2023-02-09 18:29:33 +0100

    REFACTOR - import jobOffer data

--- components/JobOffers/JobOffer.test.tsx
+++ components/JobOffers/JobOffer.test.tsx
@@ -2,25 +2,13 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import JobOffer from './JobOffer';
+import jobs from '../../assets/jobData';
 
-const mockJob = {
-  id: 2,
-  title: 'Développeur Front',
-  status: 'disponible',
-  place: 'Paris',
-  sector: 'Immobilier',
-  publishDate: Date.now(),
-  startDate: Date.now(),
-  weeklyWorkTime: '35h',
-  details:
-    'ed rutrum eros est, eu auctor lacus finibus id. Praesent leo lorem, commodo non ex vel, gravida scelerisque dolor. Nunc a enim dignissim, tincidunt magna eget, euismod ligula. Sed tellus lectus, mollis et malesuada ac, vehicula in orci. Proin ex mauris, pharetra quis malesuada in, congue non tellus. Nam et velit vel diam ultrices commodo. Nam accumsan ultricies varius. Mauris venenatis ac sem vitae commodo. Integer tempus ipsum sed sem pharetra, sed vehicula lacus maximus. Fusce ultricies ornare tellus, at feugiat nulla sagittis id. Proin scelerisque quam non lacus auctor scelerisque. Nulla facilisi. Pellentesque ut est purus.',
-  image: 'https://www.w3bai.com/w3css/img_fjords.jpg',
-  linkedin: 'https://fr.linkedin.com/',
-  visibility: true,
-};
+const mockJob = jobs[2];
 
 describe('JobOffer Component', () => {
   beforeEach(() => render(<JobOffer job={mockJob} />));
+  afterEach(() => jest.restoreAllMocks());
 
   test('Should display a h3 title with the job title', () => {
     const title = screen.getByRole('heading', { level: 3 });
