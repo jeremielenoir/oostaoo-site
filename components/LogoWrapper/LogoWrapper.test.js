1f77bbd3e49ccee2327f24870bd3fa15aa1dddb1
pablo <prietopablo88@gmail.com>
2023-02-09 11:23:38 +0100

    added src testing and presnece of elements in the document

--- components/LogoWrapper/LogoWrapper.test.js
+++ components/LogoWrapper/LogoWrapper.test.js
@@ -1,18 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import LogoWrapper from './LogosWrapper';
 
 import logoArray from '../../assets/Logos';
 
 describe('LogoWrapper component tests', () => {
   test('component should render', () => {
-    render(<LogoWrapper logos={['']} section="technos" />);
+    render(<LogoWrapper logos={['']} section="references" />);
   });
 
   beforeEach(() => render(<LogoWrapper logos={logoArray.technos} section="technos" />));
 
-  test('component should have images with the correct alt attributes', () => {
+  test('component should have the proper amount of logos', () => {
     logoArray.technos
       .map((logoElement) => screen.getByAltText(`logo ${logoElement}`))
-      .forEach((imageAlt) => expect(imageAlt).toBeInTheDocument());
+      .forEach((logoElement) => expect(logoElement).toBeInTheDocument());
+  });
+
+  test('images element should have the correct src and alt attributes', () => {
+    logoArray.technos
+      .map((logoElement) => screen.getByAltText(`logo ${logoElement}`))
+      .forEach(async (imgElement, index) => {
+        await waitFor(() => expect(imgElement).toHaveAttribute('src', `/../public/assets/technos/${logoArray.technos[index]}.png`)); // in next app there is a lazy load on image component
+        expect(imgElement).toHaveAttribute('alt', `logo ${logoArray.technos[index]}`);
+      });
   });
 });
