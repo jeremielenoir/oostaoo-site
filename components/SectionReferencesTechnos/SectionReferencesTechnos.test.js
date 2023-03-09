d93fd47fee36c48acef0115529bc99de4460fcfe
Julien-Allard <julalldev@gmail.com>
2023-03-09 16:07:34 +0100

    Fix/SectionReferencesTechnos.test.js

--- components/SectionReferencesTechnos/SectionReferencesTechnos.test.js
+++ components/SectionReferencesTechnos/SectionReferencesTechnos.test.js
@@ -1,13 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import SectionReferencesTechnos from './SectionReferencesTechnos';
 
-describe('SectionReferencesTechnos component tests', () => {
-  test('component should render', () => {
-    render(<SectionReferencesTechnos section="technos" />);
-  });
+import { getStaticProps } from '../../pages/index';
 
-  test('SectionTitle and LogosWrapper should be in the document', () => {
-    render(<SectionReferencesTechnos section="technos" />);
+describe('SectionReferencesTechnos component tests', () => {
+  test('SectionTitle and LogosWrapper should be in the document', async () => {
+    const response = await getStaticProps();
+    const technosTitle = response.props.allTitles.technos;
+    const logosArray = response.props.logosArray.technos;
+    render(
+      <SectionReferencesTechnos
+        section="technos"
+        title={technosTitle}
+        logos={logosArray}
+      />,
+    );
 
     expect(screen.getByTestId('sectionTitle')).toBeInTheDocument();
     expect(screen.getByTestId('logosWrapper')).toBeInTheDocument();
