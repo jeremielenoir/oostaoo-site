82dc9ff68d8964bb02a35f4f3266ab696c0351b6
Julien-Allard <julalldev@gmail.com>
2023-03-09 16:23:43 +0100

    Fix/Services.test.js

--- components/Services/Services.test.tsx
+++ components/Services/Services.test.tsx
@@ -2,22 +2,30 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Services from './Services';
-import servicesData from '../../assets/Services';
 import SectionTitle from '../SectionTitle/SectionTitle';
 import ServiceContainer from '../ServiceContainer/ServiceContainer';
+
+import { getStaticProps } from '../../pages/index';
 
 jest.mock('../SectionTitle/SectionTitle');
 jest.mock('../ServiceContainer/ServiceContainer');
 
 describe('Services Component', () => {
-  beforeEach(() => render(<Services />));
+  beforeEach(async () => {
+    const response = await getStaticProps();
+    const servicesData = response.props.servicesArray;
+    const servicesTitle = response.props.allTitles.services;
+    render(<Services servicesArray={servicesData} title={servicesTitle} />);
+  });
   afterEach(() => jest.clearAllMocks());
 
-  test('Should display a SectionTitle sub-component', () => {
+  test('Should display a SectionTitle sub-component', async () => {
     expect(SectionTitle).toHaveBeenCalledTimes(1);
   });
 
-  test('Should display ServiceContainer sub-component', () => {
+  test('Should display ServiceContainer sub-component', async () => {
+    const response = await getStaticProps();
+    const servicesData = response.props.servicesArray;
     expect(ServiceContainer).toHaveBeenCalledTimes(servicesData.length);
   });
 });
