2f93ec9b730fcfcba51ea083926097e9ed9572b5
Paul Rigaudeau <paul.rigaudeau@me.com>
2023-03-13 11:42:13 +0100

    FEATURE - add logger on db client connexion

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,25 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
-import type { User } from '@prisma/client';
+import type { PrismaClient, User } from '@prisma/client';
 
+import db from './database/client';
 import { verifyTokenAndGetUser } from './services/tokenManager';
 
 export type Context = {
   db: PrismaClient
   response: NextApiResponse
-  user: Omit<User, 'password'> | null | Error
+  connectedUser: Omit<User, 'password'> | null | Error
 };
 
-const db = new PrismaClient();
-
 export default async (request: NextApiRequest, response: NextApiResponse) => {
-  const token = request.cookies['auth-token'] || '';
-  const user = await verifyTokenAndGetUser(
+  const token = request.cookies['auth-token'];
+  const connectedUser = token && await verifyTokenAndGetUser(db, token);
+
+  return {
     db,
-    token,
-  );
-
-  console.log('context log');
-
-  return { db, response, user };
+    response,
+    connectedUser,
+  };
 };
