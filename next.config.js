4073988c221709b4429e719efd2d1bb0a8ce20d0
Inès LUJAN <melusyne@iness-macbook-air.home>
2023-06-26 17:01:25 +0200

    correct Apply button CSS for build and add dockerfile

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   // TODO : Temporary domain permission to remove when loading real images from backend
   images: {
